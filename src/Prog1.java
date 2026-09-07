d0519d87307439ffeb8959b3c3d363869e04329c
Sinchana S Bhat <sinchusinchusb@gmail.com>
2026-09-07 17:59:42 +0530

    file added

---
+++ src/Prog1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Prog1{
+    public static void main(String[] args) {
+        System.out.print("Enter your marks:");
+        Scanner sc = new Scanner(System.in);
+        int marks = sc.nextInt();
+        System.out.println("Your marks is:"+marks);
+    }
+}
