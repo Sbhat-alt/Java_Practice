ad035bf0f28e41e644a8f000fee4c2cae1806605
Sinchana S Bhat <sinchusinchusb@gmail.com>
2026-09-07 17:56:34 +0530

    file added

---
+++ src/Reverse.java
@@ -0,0 +1,14 @@
+public class Reverse{
+    public static void main(String[] args) {
+        int n = 1223224;
+        int res = 0;
+        while(n>0)
+        {
+            int rem = n%10;
+            res = res*10+rem;
+            n/=10;
+        }
+
+        System.out.println(res);
+    }
+}
