b96e725fce5ac0f6d68edc661a36711b11f7412c
Sinchana S Bhat <sinchusinchusb@gmail.com>
2026-09-08 22:20:35 +0530

    file added

--- src/Sum.java
+++ src/Sum.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Sum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a:");
+        int a = in.nextInt();
+        System.out.print("Enter b:");
+        int b = in.nextInt();
+        int result = sum(a,b);
+        System.out.println(result);
+    }
+    static int sum(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
 }
