392762531acc33ad5dc0fedc26482d9a64720fef
Sinchana S Bhat <sinchusinchusb@gmail.com>
2026-09-08 22:20:07 +0530

    file added

--- src/Prime.java
+++ src/Prime.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Prime {
+    public static void main(String[] args) {
+        int c = 2;
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        if(num<=1){
+            System.out.println("neither prime nor composite");
+        }
+        while(c<num){
+            if(num%c==0){
+                System.out.println("Composite number");
+                break;
+            }
+            else {c = c + 1;}
+        }
+        if(c==num){
+            System.out.println("Prime number");
+        }
+    }
 }
