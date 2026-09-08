cae230b7c3bc72c2e28fa325664c80114d7e036b
Sinchana S Bhat <sinchusinchusb@gmail.com>
2026-09-08 22:19:33 +0530

    file added

--- src/NestedSwitch.java
+++ src/NestedSwitch.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class NestedSwitch {
+    public static  void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your choice");
+        int empID = in.nextInt();
+
+
+        switch(empID){
+            case 1:
+                System.out.println("Pratap Singh");
+                break;
+            case 2:
+                System.out.println("rahul singh");
+                break;
+            case 3:
+                String department = in.next();
+
+                switch(department){
+                    case "IT":
+                        System.out.println("IT Department");
+                        break;
+                    case "Management":
+                        System.out.println("Management department");
+                        break;
+                    default:
+                        System.out.println("Invalid");
+                }
+             break;
+            default:
+                System.out.println("invalid");
+        }
+    }
 }
