5f2165a037641c7f8f7e8b4e0d4f13b5672f02e0
Sinchana S Bhat <sinchusinchusb@gmail.com>
2026-09-08 22:19:02 +0530

    file added

--- src/GreetFunction.java
+++ src/GreetFunction.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
+//public class GreetFunction {
+//    public static void main(String[] args) {
+//
+//        greet();
+//    }
+//    static void greet() {
+//        System.out.println("Good day ahead");
+//    }
+//}
 public class GreetFunction {
+    public static void main(String[] args) {
+        String result = myGreet("namasteyy");
+        System.out.println(result);
+    }
+    static String myGreet(String message)
+    {
+        String greeting = message + " Sinch";
+        return greeting;
+    }
 }
