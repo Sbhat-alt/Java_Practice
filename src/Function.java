1963c1ef0f129cb1974885b155172a35fc96c06e
Sinchana S Bhat <sinchusinchusb@gmail.com>
2026-09-08 22:18:22 +0530

    file added

---
+++ src/Function.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Function {
+    public static void main(String[] args) {
+        int result = sum();
+        System.out.println(result);
+    }
+    static int sum() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number 1:");
+        int num1 = in.nextInt();
+        System.out.print("Enter number 2:");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        return sum;
+    }
+}
